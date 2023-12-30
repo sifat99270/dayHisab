8639088492cb93f468b5b1b55c76907d05c914b2
smn <rasifat33@gmail.com>
2023-12-30 18:50:40 +0600

    king

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body >{children}</body>
-    </html>
+      <html lang="en">
+      <body className={inter.className}>{children}</body>
+      </html>
   )
 }
